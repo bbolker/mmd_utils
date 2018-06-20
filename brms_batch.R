1593b2577fba4ea817ee7382a1cdbca8c34a883a
Ben Bolker <bbolker@gmail.com>
2018-06-20 15:09:47 -0400

    update brms batch

--- brms_batch.R
+++ brms_batch.R
@@ -1,28 +1,30 @@
 source("mmd_utils.R")
 source("gamm4_utils.R")
 L <- load("ecoreg.RData")
-
-options(mc.cores=2)
+library(brms)
+options(mc.cores=3)
 
 ## use mmd_utils model-construction code?
 
-library(brms)
-system.time(b1 <- brm(mbirds_log ~ (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc)^2+
+form1 <- mbirds_log ~ (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc)^2+
         (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome)+
         (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|flor_realms)+
-        (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome_FR),
-    adapt_delta=0.99,
-    family=gaussian,
-    data=ecoreg))
-save("b1",file="brms_batch.RData")
+        (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome_FR)
 
-system.time(b2 <- brm(mbirds_log ~ (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc)^2+
-        (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome)+
-        (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|flor_realms)+
-            (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome_FR)+
-       s(y,x,bs='sos'),
-    adapt_delta=0.99,
+t1 <- system.time(b1 <- brm(form1,
+    control=list(adapt_delta=0.99),
+   family=gaussian,
+   data=ecoreg))
+
+  save("b1",file="brms_batch.RData")
+
+load("brms_batch.RData")
+form2 <- update(form1, . ~ . + s(y,x,bs="sos"))
+
+t2 <- system.time(b2 <- brm(form2,
+    control=list(adapt_delta=0.99),
     family=gaussian,
     data=ecoreg))
-save("b1","b2",file="brms_batch.RData")
+
+save("t1","t2","b1","b2",file="brms_batch.RData")
 
