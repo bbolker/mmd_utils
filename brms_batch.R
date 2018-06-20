efcb34f58f16c9503f1b9b56e345a8e7bb811546
Ben Bolker <bbolker@gmail.com>
2018-06-20 10:25:54 -0400

    adapt_delta and mc.cores for brms batch

--- brms_batch.R
+++ brms_batch.R
@@ -2,6 +2,8 @@ source("mmd_utils.R")
 source("gamm4_utils.R")
 L <- load("ecoreg.RData")
 
+options(mc.cores=2)
+
 ## use mmd_utils model-construction code?
 
 library(brms)
@@ -9,15 +11,18 @@ system.time(b1 <- brm(mbirds_log ~ (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc)^2+
         (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome)+
         (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|flor_realms)+
         (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome_FR),
+    adapt_delta=0.99,
     family=gaussian,
     data=ecoreg))
+save("b1",file="brms_batch.RData")
 
 system.time(b2 <- brm(mbirds_log ~ (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc)^2+
         (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome)+
         (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|flor_realms)+
             (NPP_log+Feat_log+NPP_cv_sc+Feat_cv_sc|biome_FR)+
        s(y,x,bs='sos'),
+    adapt_delta=0.99,
     family=gaussian,
     data=ecoreg))
-
 save("b1","b2",file="brms_batch.RData")
+
