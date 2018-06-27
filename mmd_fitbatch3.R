29b280e3ff5f815c8307abc101f7b95d9a537a21
Ben Bolker <bbolker@gmail.com>
2018-06-27 11:55:17 -0400

    tweak brms batch file

--- mmd_fitbatch3.R
+++ mmd_fitbatch3.R
@@ -8,12 +8,20 @@ options(mc.cores=3)
 ## pick out all response variables
 respvars <- c("mamph","plants","mmamm","mbirds")
 logrespvars <- paste0(respvars,"_log")
-tmpf <- function(r) fit_all(response=r,
-                            single_fit=c(3,3,3),
-                            platform="brms")
+tmpf <- function(r) {
+    res <- fit_all(response=r,
+            single_fit=c(3,3,3),
+            platform="brms")
+    return(res)
+}
 ## don't fit respvars in parallel because brm is doing parallel
 ##  chains (maybe modify if we move to SHARCnet)
-allfits_brms <- lapply(logrespvars,tmpf)
-names(allfits_brms) <- logrespvars
+## for loop rather than lapply() so we can checkpoint
 
+allfits_brms <- setNames(vector("list",length=length(logrespvars)),logrespvars)
+for (i in seq_along(allfits_brms)) {
+    allfits_brms[[i]] <- tmpf(logrespvars[i])
+    ## checkpoint
+    save("allfits_brms",file="allfits_brms.RData")
+}
 save("allfits_brms",file="allfits_brms.RData")
